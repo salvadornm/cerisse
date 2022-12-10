7ca35dad5ecbd111190928b97502d92110a6d276
ensonun <53259417+ensonun@users.noreply.github.com>
2022-12-10 01:09:17 +0000

    changed speed of sound in characteristic recon

--- EB_CNS/Source/advance_box.cpp
+++ EB_CNS/Source/advance_box.cpp
@@ -69,8 +69,8 @@ using namespace amrex;
 
 void
 CNS::compute_dSdt_box (const Box& bx,
-                       Array4<Real const>& sfab,
-                       Array4<Real      >& dsdtfab,
+                       Array4<Real const>& sarr,
+                       Array4<Real      >& dsdtarr,
                        const std::array<FArrayBox*, AMREX_SPACEDIM>& flxfab
                        //FArrayBox           flxfab[AMREX_SPACEDIM]
                        /*AMREX_D_DECL(
@@ -80,7 +80,6 @@ CNS::compute_dSdt_box (const Box& bx,
 {
     BL_PROFILE("CNS::compute_dSdt_box()");
     
-    const Box& bxg1 = amrex::grow(bx,1);
     const Box& bxg2 = amrex::grow(bx,2);
     const Box& bxg3 = amrex::grow(bx,3);
     
@@ -88,20 +87,17 @@ CNS::compute_dSdt_box (const Box& bx,
     
     // Primitive variables
     FArrayBox qtmp(bxg3, LEN_PRIM);
-    auto const& q = qtmp.array();
 
     Parm const* lparm = d_parm;
-    // for (int nf = 0; nf <= NUM_FIELD; ++nf){
-    //     auto const& qfill = qtmp.array(nf*NPRIM);
-    amrex::ParallelFor(bxg3,
-    [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept {
-        cns_ctoprim(i, j, k, sfab, q, *lparm);
-    });
-    // }
+    for (int nf = 0; nf <= NUM_FIELD; ++nf){
+        auto const& qfill = qtmp.array(nf*NPRIM);
+        amrex::ParallelFor(bxg3,
+        [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept {
+            cns_ctoprim(i, j, k, nf*NVAR, sarr, qfill, *lparm);
+        });
+    }
 
-    // Slopes
-    // FArrayBox slopetmp(bxg1, 5+NUM_SPECIES);
-    // auto const& slope = slopetmp.array();
+    // Arrays for characteristic reconstruction
     FArrayBox wtmp(bxg3, NCHAR);
     FArrayBox wl_tmp(bxg2, NCHAR);
     FArrayBox wr_tmp(bxg2, NCHAR);
@@ -112,83 +108,177 @@ CNS::compute_dSdt_box (const Box& bx,
     // Transport coef
     FArrayBox diff_coeff;
     if (do_visc == 1) {
-        diff_coeff.resize(bxg2, NCOEF);
-       
-        auto const& qar_yin   = qtmp.array(QFS);
-        auto const& qar_Tin   = qtmp.array(QTEMP);
-        auto const& qar_rhoin = qtmp.array(QRHO);
-        auto const& rhoD   = diff_coeff.array(CRHOD);
-        auto const& mu     = diff_coeff.array(CMU);
-        auto const& xi     = diff_coeff.array(CXI);
-        auto const& lambda = diff_coeff.array(CLAM);
-        
-        BL_PROFILE("PelePhysics::get_transport_coeffs()");
-        // Get Transport coefs on GPU.
-        auto const* ltransparm = trans_parms.device_trans_parm();
-        amrex::launch(bxg2, [=] AMREX_GPU_DEVICE(amrex::Box const& tbx) {
-            auto trans = pele::physics::PhysicsType::transport();
-            trans.get_transport_coeffs(
-                tbx, qar_yin, qar_Tin, qar_rhoin, rhoD, mu, xi, lambda, ltransparm);
-        });
-    }
+        diff_coeff.resize(bxg2, LEN_COEF);
 
-    // Loop directions
-    for (int cdir = 0; cdir < AMREX_SPACEDIM; ++cdir) {
-        // Convert primitive to characteristic
-        amrex::ParallelFor(bxg3,
-        [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept {
-            cns_ctochar(i, j, k, cdir, q, w);
-        });
-
-        // Reconstruction
-        const Box& qlrbox = amrex::grow(bx,cdir,1);
-        amrex::ParallelFor(qlrbox, NCHAR,
-        [=] AMREX_GPU_DEVICE (int i, int j, int k, int n) noexcept {
-            cns_recon(i, j, k, n, cdir, w, wl, wr, recon_scheme, plm_theta);
-        });
-
-        // Solve Riemann problem
-        const Box& flxbx = amrex::surroundingNodes(bx,cdir);
-        auto const& flx_arr = flxfab[cdir]->array();
-        amrex::ParallelFor(flxbx,
-        [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept {
-            cns_riemann(i, j, k, cdir, flx_arr, q, wl, wr, *lparm);
-            for (int n = UFA; n < NUM_AUX; ++n) flx_arr(i,j,k,n) = Real(0.0);
-        });
-
-        // Get viscous fluxes
-        if (do_visc == 1) {
-            auto const& coefs = diff_coeff.array();
-            amrex::ParallelFor(flxbx,
-            [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept {
-                cns_diff(i, j, k, cdir, q, coefs, dxinv, flx_arr);
+        for (int nf = 0; nf <= NUM_FIELD; ++nf){
+            auto const& qar_yin   = qtmp.array(nf*NPRIM + QFS);
+            auto const& qar_Tin   = qtmp.array(nf*NPRIM + QTEMP);
+            auto const& qar_rhoin = qtmp.array(nf*NPRIM + QRHO);
+            auto const& mu     = diff_coeff.array(nf*NCOEF + CMU);
+            auto const& xi     = diff_coeff.array(nf*NCOEF + CXI);
+            auto const& lambda = diff_coeff.array(nf*NCOEF + CLAM);
+            auto const& rhoD   = diff_coeff.array(nf*NCOEF + CRHOD);
+            
+            BL_PROFILE("PelePhysics::get_transport_coeffs()");
+            // Get Transport coefs on GPU.
+            auto const* ltransparm = trans_parms.device_trans_parm();
+            amrex::launch(bxg2, [=] AMREX_GPU_DEVICE(amrex::Box const& tbx) {
+                auto trans = pele::physics::PhysicsType::transport();
+                trans.get_transport_coeffs(
+                    tbx, qar_yin, qar_Tin, qar_rhoin, rhoD, mu, xi, lambda, ltransparm);
             });
         }
     }
+
+    // A fab to store the viscous fluxes in VPDF or VSPDF
+    FArrayBox v_flux_fab;
+    auto const& vflx_arr = v_flux_fab.array();
     
-    amrex::ParallelFor(bx, NVAR,
-    [=] AMREX_GPU_DEVICE (int i, int j, int k, int n) noexcept
-    {
-        cns_div(i, j, k, n, dsdtfab, AMREX_D_DECL(flxfab[0]->array(),flxfab[1]->array(),flxfab[2]->array()), dxinv);
+    for (int cdir = 0; cdir < AMREX_SPACEDIM; ++cdir) { // Loop through space direction
+        const Box& flxbx = amrex::surroundingNodes(bx,cdir);
+        const Box& reconbox = amrex::grow(bx,cdir,1);
+        if (do_visc == 1) {
+            v_flux_fab.resize(flxbx, NVAR);
+            v_flux_fab.setVal(0.0);
+        }
+
+        for (int nf = 0; nf <= NUM_FIELD; ++nf) {       // Loop through fields
+            // Convert primitive to characteristic
+            auto const& q = qtmp.array(nf*NPRIM);
+            amrex::ParallelFor(bxg3,
+            [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept {
+                cns_ctochar(i, j, k, cdir, q, w, 0);
+            });
+
+            // Reconstruction            
+            amrex::ParallelFor(reconbox, NCHAR,
+            [=] AMREX_GPU_DEVICE (int i, int j, int k, int n) noexcept {
+                cns_recon(i, j, k, n, cdir, w, wl, wr, recon_scheme, plm_theta);
+            });
+
+            // Solve Riemann problem, store advection and pressure fluxes to flx_arr
+            auto const& flx_arr = flxfab[cdir]->array(nf*NVAR);
+            amrex::ParallelFor(flxbx,
+            [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept {
+                cns_riemann(i, j, k, cdir, flx_arr, /*nf > 1 ? pflx_arr :*/ flx_arr, q, wl, wr, 0, *lparm);
+            // });
+
+            // auto const& q = qtmp.array(nf*NPRIM);
+            // auto const& flx_arr = flxfab[cdir]->array(nf*NVAR);
+            // amrex::ParallelFor(flxbx,
+            // [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept {
+            //     recon_riemann_aio(i, j, k, cdir, q, flx_arr, /*nf > 1 ? pflx_arr :*/ flx_arr, recon_scheme, plm_theta, *lparm);
+            
+            // amrex::ParallelFor(flxbx,
+            // [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept {
+                amrex::Real check_rho_flx = 0.0;
+                for (int n = 0; n < NUM_SPECIES; ++n) {
+                    check_rho_flx += flx_arr(i, j, k, UFS+n);
+                }
+                if (std::abs(check_rho_flx - flx_arr(i, j, k, URHO)) > 1e-20) {
+                    amrex::Print() << i << " in bx " << bx;
+                    amrex::Print() << ": sum(rY_flx) = " << check_rho_flx << ", sum(rho_flx) = " << flx_arr(i, j, k, URHO) << std::endl;
+                    amrex::Print() << flx_arr(i, j, k, UFS) << " " << flx_arr(i, j, k, UFS+1) << std::endl;
+                                       
+                    amrex::Real yl0 = wl(i, j, k, WY);
+                    amrex::Real yl1 = wl(i, j, k, WY+1);
+                    amrex::Real rl = wl(i, j, k, WRHO) + (wl(i, j, k, WACO) + wl(i, j, k, WACO+1)) / q(i-1, j, k, QC);
+                    amrex::Print() << yl0 << " " << yl1 << " " << rl << std::endl;
+                    // amrex::Error();
+                }
+            });
+
+            // Store viscous fluxes separately
+            if (do_visc == 1) {
+                auto const& coefs = diff_coeff.array(nf*NVAR);
+                amrex::ParallelFor(flxbx,
+                [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept {
+                    cns_diff(i, j, k, cdir, q, coefs, dxinv, nf > 0 ? vflx_arr : flx_arr);
+                });
+            }
+            
+            // amrex::Print() << "dir = " << cdir << " nf = " << nf << ": ";
+            // for (int n = 0; n < NVAR; ++n)
+            //    amrex::Print() << pflx_arr(lbound(bx).x,0,0,n) << " ";
+            // amrex::Print() << std::endl;
+        }
+        
+        // // Zero aux variable fluxes (no need. fluxes are zeroed at initialisation)
+        // amrex::ParallelFor(flxbx,
+        // [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept {
+        //     for (int na = 0; na < NUM_AUX; ++na) flx_arr(i,j,k,UFA+na) = Real(0.0);
+        // });
+
+        if ((NUM_FIELD > 0) && (do_visc == 1)) {
+            auto const& flx_arr = flxfab[cdir]->array();
+            amrex::ParallelFor(flxbx,
+            [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept {
+                // Average the viscous fluxes
+                AMREX_D_TERM(
+                    vflx_arr(i, j, k, UMX) /= amrex::Real(NUM_FIELD); ,
+                    vflx_arr(i, j, k, UMY) /= amrex::Real(NUM_FIELD); ,
+                    vflx_arr(i, j, k, UMZ) /= amrex::Real(NUM_FIELD););
+                vflx_arr(i, j, k, UEDEN) /= amrex::Real(NUM_FIELD);
+                
+                // Add to hyperbolic fluxes
+                for (int nf = 1; nf <= NUM_FIELD; ++nf) {
+                    AMREX_D_TERM(
+                        flx_arr(i, j, k, nf*NVAR + UMX) += vflx_arr(i, j, k, UMX); ,
+                        flx_arr(i, j, k, nf*NVAR + UMY) += vflx_arr(i, j, k, UMY); ,
+                        flx_arr(i, j, k, nf*NVAR + UMZ) += vflx_arr(i, j, k, UMZ););
+                    flx_arr(i, j, k, nf*NVAR + UEDEN) += vflx_arr(i, j, k, UEDEN);
+                }
+            });
+        }
+
+        // amrex::Print() << "bx = " << flxbx << ": ";
+        // for (int n = 0; n <= NUM_FIELD; ++n) {
+        // for (int i = 0; i < NVAR; ++i) {
+        //     amrex::Print() << flx_arr(0, 0, 0, n*NVAR + i) << " ";
+        // }
+        // amrex::Print() << "| ";
+        // }
+        // amrex::Print() << std::endl;
+
+    }
+
+    // Compute flux divergence
+    amrex::ParallelFor(bx, LEN_STATE,
+    [=] AMREX_GPU_DEVICE (int i, int j, int k, int n) noexcept {
+        cns_div(i, j, k, n, dsdtarr, AMREX_D_DECL(flxfab[0]->array(),flxfab[1]->array(),flxfab[2]->array()), dxinv);
     });
 
-//     if (do_ext_src) {
-//     ...
-//         const Real g = gravity;
-//         const int irho = URHO;
-// #if (AMREX_SPACEDIM == 2)
-//         const int imz = UMY;
-// #elif (AMREX_SPACEDIM == 3)
-//         const int imz = UMZ;
-// #endif
-//         const int irhoE = UEDEN;
-//         amrex::ParallelFor(bx,
-//         [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
-//         {
-//             dsdtfab(i,j,k,imz  ) += g * sfab(i,j,k,irho);
-//             dsdtfab(i,j,k,irhoE) += g * sfab(i,j,k,imz);
-//         });
-//     }
+    // Add external source term
+    if (do_ext_src) {
+        Parm const* lparm = d_parm;
+        ProbParm const* lprobparm = d_prob_parm;
+        const auto geomdata = geom.data();
+        const amrex::Real time = state[State_Type].curTime();
+
+        amrex::ParallelFor(bx,
+        [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept {
+            fill_ext_src(i, j, k, time, geomdata, sarr, dsdtarr, *lparm, *lprobparm);
+        });
+    }
+
+    // Average dsdt to mean
+    // if (NUM_FIELD > 0) {
+    //     const amrex::Real NFinv = 1.0/amrex::Real(NUM_FIELD);
+    //     amrex::ParallelFor(bx,
+    //     [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept {            
+    //         for (int n = 0; n < NVAR; ++n) {
+    //             dsdtarr(i, j, k, n) = 0.0;
+    //             for (int nf = 1; nf <= NUM_FIELD; ++nf) {
+    //                 dsdtarr(i, j, k, n) += NFinv * dsdtarr(i, j, k, nf*NVAR + n);
+    //             }
+    //         }
+    //     });
+    // }
+
+    // Add turbulence model
+    // if (NUM_FIELD > 0) {
+    //     vpdf_langevin_model(bx, sarr, dsdtarr, dWdt, dxinv);
+    // }
 
     Gpu::streamSynchronize();
 }
