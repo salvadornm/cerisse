f47b929cff7e7c0cdcd6d55cf8a8073b6a6ec2fc
Monal <monal-@hotmail.com>
2023-02-24 20:29:45 +0000

    update cgal installation

--- lib/install_cgal.sh
+++ lib/install_cgal.sh
@@ -1,11 +1,11 @@
+#https://www.boost.org/doc/libs/1_66_0/more/getting_started/unix-variants.html#easy-build-and-install
+cd ./boost
+/bootstrap.sh --prefix=/home/monal/code/cfd/amrsolver/lib/install/boost
+./b2 install
+cd ..
+
 cd ./cgal
 mkdir build
 cd ./build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/home/monal/code/cfd/amrsolver/lib/install/cgal ..
 make install
-
-
-#https://www.boost.org/doc/libs/1_66_0/more/getting_started/unix-variants.html#easy-build-and-install
-#cd ./boost
-#./bootstrap.sh --prefix=
-#./b2 install
