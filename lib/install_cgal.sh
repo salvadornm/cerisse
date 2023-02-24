d38442bd62e8e4312590b6c57802a803e3d8136c
Monal <monal-@hotmail.com>
2023-02-24 13:16:18 +0000

    add boost install lines (commented out)

--- lib/install_cgal.sh
+++ lib/install_cgal.sh
@@ -4,5 +4,8 @@ cd ./build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/home/monal/code/cfd/amrsolver/lib/install/cgal ..
 make install
 
+
+#https://www.boost.org/doc/libs/1_66_0/more/getting_started/unix-variants.html#easy-build-and-install
 #cd ./boost
 #./bootstrap.sh --prefix=
+#./b2 install
