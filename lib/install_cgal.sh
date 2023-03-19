706d14b64a9ec69352fe0f6e36f2df955b574d52
Monal-Patel <monal-@hotmail.com>
2023-03-19 19:25:16 +0000

    repo initial setup changes

--- lib/install_cgal.sh
+++ lib/install_cgal.sh
@@ -1,6 +1,9 @@
 cd ../
 dir=$PWD
 
+file=$HOME/.bashrc
+echo "export AMREX_HOME=$dir/lib/amrex" >> $file
+
 #https://www.boost.org/doc/libs/1_66_0/more/getting_started/unix-variants.html#easy-build-and-install
 cd $dir/lib/boost
 ./bootstrap.sh --prefix=$dir/lib/install/boost
