53759202bbc7c63afd1d97ffc513e0400fd7975e
ensonun <53259417+ensonun@users.noreply.github.com>
2023-06-06 19:11:26 +0100

    add hypersonic flat plate case

---
+++ EB_CNS/Exec/FlatPlate/Null2/mechanism.cpp
@@ -0,0 +1,101 @@
+#ifndef MECHANISM_CPP
+#define MECHANISM_CPP
+
+#include "mechanism.H"
+
+void
+CKAWT(amrex::Real* /*awt*/)
+{
+}
+
+void
+CKNCF(int* /*ncf*/)
+{
+}
+
+void
+CKSYME_STR(amrex::Vector<std::string>& ename)
+{
+  ename.resize(1);
+  ename[0] = "XO";
+}
+void
+CKSYMS_STR(amrex::Vector<std::string>& kname)
+{
+  kname.resize(1);
+  kname[0] = "XO";
+}
+
+// compute sparsity pattern of the chemistry Jacobian
+void
+SPARSITY_INFO(int* /*nJdata*/, const int* /*consP*/, int /*NCELLS*/)
+{
+}
+
+// compute sparsity pattern of the system Jacobian
+void
+SPARSITY_INFO_SYST(int* /*nJdata*/, const int* /*consP*/, int /*NCELLS*/)
+{
+}
+
+// compute sparsity pattern of the simplified (for preconditioning) system
+// Jacobian
+void
+SPARSITY_INFO_SYST_SIMPLIFIED(int* /*nJdata*/, const int* /*consP*/)
+{
+}
+
+// compute sparsity pattern of the chemistry Jacobian in CSC format -- base 0
+void
+SPARSITY_PREPROC_CSC(
+  int* /*rowVals*/
+  ,
+  int* /*colPtrs*/,
+  const int* /*consP*/,
+  int /*NCELLS*/)
+{
+}
+
+// compute sparsity pattern of the chemistry Jacobian in CSR format -- base 0
+void
+SPARSITY_PREPROC_CSR(
+  int* /*colVals*/,
+  int* /*rowPtrs*/,
+  const int* /*consP*/,
+  int /*NCELLS*/,
+  int /*base*/)
+{
+}
+
+// compute sparsity pattern of the system Jacobian
+// CSR format BASE is user choice
+void
+SPARSITY_PREPROC_SYST_CSR(
+  int* /*colVals*/,
+  int* /*rowPtr*/,
+  const int* /*consP*/,
+  int /*NCELLS*/,
+  int /*base*/)
+{
+}
+
+// compute sparsity pattern of the simplified (for precond) system Jacobian on
+// CPU BASE 0
+void
+SPARSITY_PREPROC_SYST_SIMPLIFIED_CSC(
+  int* /*rowVals*/, int* /*colPtrs*/, int* /*indx*/, const int* /*consP*/)
+{
+}
+
+// compute sparsity pattern of the simplified (for precond) system Jacobian
+// CSR format BASE is under choice
+void
+SPARSITY_PREPROC_SYST_SIMPLIFIED_CSR(
+  int* /*colVals*/
+  ,
+  int* /*rowPtr*/,
+  const int* /*consP*/,
+  int /*base*/)
+{
+}
+#endif
---
+++ EB_CNS/Exec/FlatPlate/prob.cpp
@@ -0,0 +1,52 @@
+#include "prob.H"
+
+using namespace amrex;
+
+extern "C" {
+void amrex_probinit (const int* /*init*/,
+                     const int* /*name*/,
+                     const int* /*namelen*/,
+                     const Real* /*problo*/,
+                     const Real* /*probhi*/)
+{
+    Real M = 6.85, Re = 5e5, Pr = 0.72;
+    {
+        ParmParse pp("prob");
+        pp.query("M", M);
+        pp.query("Tw", CNS::h_prob_parm->Tw);
+    }
+
+    Real e, c;
+    Real massfrac[NUM_SPECIES] = {1.0};
+    auto eos = pele::physics::PhysicsType::eos();
+    eos.PYT2RE(CNS::h_prob_parm->p, massfrac, CNS::h_prob_parm->T,
+               CNS::h_prob_parm->rho, e);
+    CNS::h_prob_parm->rhoe = CNS::h_prob_parm->rho * e;
+    eos.RTY2Cs(CNS::h_prob_parm->rho, CNS::h_prob_parm->T, massfrac, c);
+    CNS::h_prob_parm->u = M * c;
+
+    Gpu::copy(Gpu::hostToDevice, CNS::h_prob_parm, CNS::h_prob_parm+1,
+              CNS::d_prob_parm);
+
+    auto& trans_parm = CNS::trans_parms.host_trans_parm();
+    trans_parm.viscosity_mu_ref = CNS::h_prob_parm->rho * CNS::h_prob_parm->u * 20.5 / Re; // length of plate = 20.5cm
+    trans_parm.viscosity_T_ref = CNS::h_prob_parm->T;
+    trans_parm.viscosity_S = 110.4;
+    trans_parm.Prandtl_number = Pr;
+    trans_parm.const_bulk_viscosity = 0.0;
+    trans_parm.const_diffusivity = 0.0;
+
+    CNS::trans_parms.sync_to_device();
+}
+}
+
+void
+CNS::fill_ext_src (int i, int j, int k, 
+                   Real time,
+                   GeometryData const& geomdata, 
+                   Array4<const Real> const& /*state*/, 
+                   Array4<Real> const& ext_src, 
+                   Parm const& /*parm*/,
+                   ProbParm const& pp)
+{
+}
