15d5d2b7f8bdb322f102ca83def45610ab9d1464
ensonun <53259417+ensonun@users.noreply.github.com>
2023-11-14 18:22:44 +0000

    changes to problem settings

--- EB_CNS/Exec/MichiganScramjet/prob.cpp
+++ EB_CNS/Exec/MichiganScramjet/prob.cpp
@@ -12,28 +12,60 @@ void amrex_probinit(const int* /*init*/, const int* /*name*/, const int* /*namel
   Real p0 = 590e4; // total pressure [Ba]
   Real M = 2.2; // inflow Mach number
   Real T0;
+  bool micka_fuel_cond = false; // use p0, T0 or p, T
   {
     ParmParse pp("prob");
     pp.get("T0", T0); // total temperature [K]
     pp.get("spark", CNS::h_prob_parm->spark); // spark plug (modelled as heating wall)
+    pp.query("M", M);
     pp.query("record_statistics", CNS::h_prob_parm->record_statistics);
     pp.query("clean_aux_on_restart", CNS::h_prob_parm->clean_aux_on_restart);
+    pp.query("do_bl", CNS::h_prob_parm->do_bl);
+    pp.query("make_bl_on_restart", CNS::h_prob_parm->make_bl_on_restart);
+    pp.query("micka_fuel_cond", micka_fuel_cond);
   }
   CNS::h_prob_parm->Y[O2_ID] = 0.244;
   CNS::h_prob_parm->Y[N2_ID] = 0.671;
   CNS::h_prob_parm->Y[H2O_ID] = 0.085;
 
-  // Iterate to find gamma
   Real gamma = 1.313, rho, T, p;
-  for (int iter = 0; iter < 3; ++ iter) {
-    T = T0 / (1 + 0.5 * (gamma - 1) * M * M);
-    p = p0 * std::pow(1 + 0.5 * (gamma - 1) * M * M, -gamma / (gamma - 1));
 
-    eos.PYT2R(p, CNS::h_prob_parm->Y.begin(), T, rho);
-    eos.RTY2G(rho, T, CNS::h_prob_parm->Y.begin(), gamma);
+  if (M > 1.0) {
+    // Isentropic relations
+    // Iterate to find gamma
+    for (int iter = 0; iter < 3; ++ iter) {
+      T = T0 / (1 + 0.5 * (gamma - 1) * M * M);
+      p = p0 * std::pow(1 + 0.5 * (gamma - 1) * M * M, -gamma / (gamma - 1));
+
+      eos.PYT2R(p, CNS::h_prob_parm->Y.begin(), T, rho);
+      eos.RTY2G(rho, T, CNS::h_prob_parm->Y.begin(), gamma);
+    }
+  } else {
+    // Constant total enthaply
+    {
+      ParmParse pp("prob");
+      pp.get("p", p);
+    }
+    // Iterate to find gamma and cv
+    Real cv = 8.3163e+06, R; // guesses
+    T = T0 / 2; // guesses
+    for (int iter = 0; iter < 3; ++ iter) {
+      eos.PYT2R(p, CNS::h_prob_parm->Y.begin(), T, rho);
+
+      R = cv * (gamma - 1.0);
+      T = (rho * cv * T0 + p0 - p) / (cv + gamma * R * M * M);
+
+      eos.RTY2G(rho, T, CNS::h_prob_parm->Y.begin(), gamma);
+      eos.RTY2Cv(rho, T, CNS::h_prob_parm->Y.begin(), cv);
+
+      amrex::Print() << "Guess " << iter << ": (T, rho, gamma, cv) = " << T << ", " << rho << ", " << gamma << ", " << cv << '\n';
+    }
   }
   amrex::Print() << "Inflow (gamma, T, p) = " << gamma << ", " << T << ", " << p << '\n';
   // Real p = 55.410e4;
+  amrex::Real cv;
+  eos.TY2Cv(T, CNS::h_prob_parm->Y.begin(), cv);
+  CNS::h_prob_parm->cv_Tinf = cv * T;
 
   eos.PYT2RE(p, CNS::h_prob_parm->Y.begin(), T, CNS::h_prob_parm->rho, CNS::h_prob_parm->ei);
 
@@ -41,25 +73,32 @@ void amrex_probinit(const int* /*init*/, const int* /*name*/, const int* /*namel
   CNS::h_prob_parm->u = M * cs;
   
   // Fuel conditions
-  p0 = 845.0e4 + (755.e4 - 845.e4) * (T0 - 1100.0) / 300.0; // linearly varying
-  M = 1.0;
-  T0 = 288.0;
-
   CNS::h_prob_parm->Y_jet[H2_ID] = 1.0;
 
-  gamma = 1.405, rho, T, p;
-  for (int iter = 0; iter < 3; ++ iter) {
-    T = T0 / (1 + 0.5 * (gamma - 1));
-    p = p0 * std::pow(1 + 0.5 * (gamma - 1), -gamma / (gamma - 1));
-
+  if (micka_fuel_cond) {
+    p0 = 845.0e4 + (755.e4 - 845.e4) * (T0 - 1100.0) / 300.0; // linearly varying
+    M = 1.0;
+    T0 = 288.0;
+    gamma = 1.405;
+    for (int iter = 0; iter < 3; ++ iter) {
+      T = T0 / (1 + 0.5 * (gamma - 1));
+      p = p0 * std::pow(1 + 0.5 * (gamma - 1), -gamma / (gamma - 1));
+      eos.PYT2R(p, CNS::h_prob_parm->Y_jet.begin(), T, rho);
+      eos.RTY2G(rho, T, CNS::h_prob_parm->Y_jet.begin(), gamma);
+    }
+  } else {
+    // Rebeiro
+    p = 845.0e4 + (755.e4 - 845.e4) * (T0 - 1100.0) / 300.0; // linearly varying
+    M = 1.0;
+    T = 288.0;
     eos.PYT2R(p, CNS::h_prob_parm->Y_jet.begin(), T, rho);
     eos.RTY2G(rho, T, CNS::h_prob_parm->Y_jet.begin(), gamma);
   }
   amrex::Print() << "Fuel (gamma, T, p) = " << gamma << ", " << T << ", " << p << '\n';
 
   eos.PYT2RE(p, CNS::h_prob_parm->Y_jet.begin(), T, CNS::h_prob_parm->rho_j, CNS::h_prob_parm->ei_j);
-
-  cs = std::sqrt(gamma * p / CNS::h_prob_parm->rho_j);
+  eos.RTY2Cs(CNS::h_prob_parm->rho_j, T, CNS::h_prob_parm->Y_jet.begin(), cs);
+  // cs = std::sqrt(gamma * p / CNS::h_prob_parm->rho_j);
   CNS::h_prob_parm->v_j = M * cs;
 
   Real mdot_air = CNS::h_prob_parm->rho * CNS::h_prob_parm->u * 3.81 * 2.54;
@@ -89,9 +128,9 @@ void Scramjet::build(const Geometry& geom, const int max_coarsening_level)
 
   auto rear_wall = EB2::PlaneIF({AMREX_D_DECL(9.525, 0.0, 0.0)}, {AMREX_D_DECL(1.0, 0.0, 0.0)});
   auto floor_wall = EB2::PlaneIF({AMREX_D_DECL(0.0, -10.0, 0.0)}, {AMREX_D_DECL(0.0, 1.0, 0.0)});
-  auto inclined_wall = EB2::PlaneIF({AMREX_D_DECL(9.53, 0.0, 0.0)}, {AMREX_D_DECL(-sin(4.8 / 180.0 * M_PI), -cos(4.8 / 180.0 * M_PI), 0.0)});
+  auto inclined_wall = EB2::PlaneIF({AMREX_D_DECL(9.525, 0.0, 0.0)}, {AMREX_D_DECL(-sin(4.0 / 180.0 * M_PI), -cos(4.0 / 180.0 * M_PI), 0.0)});
   auto triangle = EB2::IntersectionIF<EB2::PlaneIF, EB2::PlaneIF, EB2::PlaneIF>(rear_wall, floor_wall, inclined_wall);
-  
+    
   // auto all_objs = EB2::UnionIF<EB2::BoxIF, EB2::CylinderIF, 
   //                 EB2::IntersectionIF<EB2::PlaneIF, EB2::PlaneIF, EB2::PlaneIF>>(box, injector, triangle);
   auto all_objs = EB2::UnionIF<EB2::DifferenceIF<EB2::BoxIF, EB2::CylinderIF>, 
