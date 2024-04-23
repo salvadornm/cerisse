4ba7aa8d000b1d03a78e292650530d4a66307d14
Monal Patel <monal-@hotmail.com>
2024-04-23 11:41:28 -0400

    ib functional on CPU and GPU

--- src/CNS.cpp
+++ src/CNS.cpp
@@ -311,7 +311,7 @@ void CNS::computeNewDt(int finest_level, int sub_cycle, Vector<int> &n_cycle,
   // Print
   if (ParallelDescriptor::IOProcessor()) {
     for (int i = 0; i <= finest_level; i++) {
-      printf("[computeInitialDt] Level %d, Max CFL= %f, Max eigenvalues (x,y,z) = (%f,%f,%f) \n", i, CFL_level[i],eigenvals_level[i][0], eigenvals_level[i][1], eigenvals_level[i][2]);
+      printf("[computeNewDt] Level %d, Max CFL= %f, Max eigenvalues (x,y,z) = (%f,%f,%f) \n", i, CFL_level[i],eigenvals_level[i][0], eigenvals_level[i][1], eigenvals_level[i][2]);
     }
   }
 }
