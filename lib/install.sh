9691f47a78992bf054c80947661ba3aad85fb8f2
Monal Patel <monal-@hotmail.com>
2024-05-05 09:36:30 -0400

    add autodiff and clad

--- lib/install.sh
+++ lib/install.sh
@@ -26,36 +26,44 @@ case  $1 in
 		echo " installing AMREX release version .." $AMREXVERSION
 		wget https://github.com/AMReX-Codes/amrex/archive/refs/tags/$AMREXVERSION.zip
 		unzip $AMREXVERSION.zip
-        mv amrex-$AMREXVERSION amrex
+    mv amrex-$AMREXVERSION amrex
 		rm $AMREXVERSION.zip	
 		;;
 	pelephys)
 		rm -rf PelePhysics
-        echo " installing PelePhysics release version .." $PELEPVERSION
-        wget https://github.com/AMReX-Combustion/PelePhysics/archive/refs/tags/v$PELEPVERSION.zip        
-        unzip v$PELEPVERSION.zip
-        mv PelePhysics-$PELEPVERSION PelePhysics 
+    echo " installing PelePhysics release version .." $PELEPVERSION
+    wget https://github.com/AMReX-Combustion/PelePhysics/archive/refs/tags/v$PELEPVERSION.zip        
+    unzip v$PELEPVERSION.zip
+    mv PelePhysics-$PELEPVERSION PelePhysics 
 		rm v$PELEPVERSION.zip
 		;;
 	cgal)
-    	mkdir -p install
-		#echo " installing boost 1.66 version .." 
-    	cd $PWD/boost
-    	./bootstrap.sh --prefix=$PWD/../install/boost
-    	./b2 install
+    mkdir -p install
+    cd $PWD/boost
+    ./bootstrap.sh --prefix=$PWD/../install/boost
+    ./b2 install
 
-		#rm -rf cgal
-		#echo " installing CGAL release version .." $CGALVERSION
-		#wget https://github.com/CGAL/cgal/releases/download/v$CGALVERSION/CGAL-$CGALVERSION.zip		
-		#unzip CGAL-$CGALVERSION.zip	
-		#mv CGAL-$CGALVERSION cgal
-		#rm CGAL-$CGALVERSION.zip	
-		cd ../cgal
-		mkdir -p build
-		cd build
-		cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PWD/../../install/cgal ..
-		make install
+    cd ../cgal
+    mkdir -p build
+    cd build
+    cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PWD/../../install/cgal ..
+    make install
 		;;
+    autodiff)
+      git submodule update --init --recursive autodiff
+      rm -rf build/autodiff
+      mkdir -p ./build/autodiff
+      cd build/autodiff
+      cmake ../../autodiff/ -DCMAKE_INSTALL_PREFIX=../../install/autodiff
+    ;;
+    clad)
+      git submodule update --init --recursive clad
+      mkdir -p build/clad
+      mkdir -p install/clad
+      cd build/clad
+      cmake ../../clad/ -DClang_DIR=/usr/lib/llvm-11 -DLLVM_DIR=/usr/lib/llvm-11 -DCMAKE_INSTALL_PREFIX=../../install/clad -DLLVM_EXTERNAL_LIT="$(which lit)"
+      make && make install
+    ;;
     *)
 	echo " no option selected [git/safe/amrex/pelephys/cgal]"
     exit
