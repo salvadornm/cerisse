c1ae2ddaca23e81917321a888a34b4a93c995d50
Salvador <s.navarro@imperial.ac.uk>
2025-05-19 22:59:19 +0200

    MMS 3D Euler

---
+++ exm/mms/euler3d/checkorder.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# List of meshes to run
+MESH=( 
+	"16"
+	"32"
+	"64"
+	"128"
+)
+
+RUN="./main1d.gnu.ex"
+
+echo "Checking  Spatial Order Euler ..."
+echo "  "
+echo "  "
+
+rm -rf plot*
+# run and create directories 
+for grid in "${MESH[@]}"; do
+   $RUN input_dir/inputs$grid
+    mv plot plot$grid
+    printf " run  mesh with %s nodes \n "  "$grid"
+done
+
+# plot order (optional)
+#python checkorder.py
