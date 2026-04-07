de3116df3aa143e8e848741d20ac7b1fb3aa0d52
Salvador Navarro <snavarro@unizar.es>
2026-04-07 12:47:31 +0200

    mod script to fix clear error in failed ODE integration

--- bin/pp_modify.sh
+++ bin/pp_modify.sh
@@ -4,27 +4,40 @@
 
 # cd "$(dirname "$0")" || exit 1
 
+# for PELEPHYSICS25
+
 echo "... Modifying PelePhysics/Source/PelePhysicsConstraints.H"
-FILE="../lib/PelePhysics/Source/PelePhysicsConstraints.H"
-sed -i '30,35{
+FILE="./lib/PelePhysics/Source/PelePhysicsConstraints.H"
+sed -i '39,43{
   s|^[[:space:]]*//|//|;       # Keep already-commented lines as is
   t done
   s|^|//|
   :done
 }' "$FILE"
 
-echo "... Modifying PelePhysics/Reactions/ReactorCvodeUtils.cpp"
-FILE="../lib/PelePhysics/Reactions/ReactorCvodeUtils.cpp"
-sed -i '16{
+echo "... Modifying PelePhysics/Source/Reactions/ReactorCvodePreconditioner.cpp"
+FILE="./lib/PelePhysics/Source/Reactions/ReactorCvodePreconditioner.cpp"
+sed -i '115{/cuS_st/{ # Keep already-commented lines as is
+  s/.*/#ifdef AMREX_USE_FLOAT\
+    cuS_st = cusolverSpScsrqrsvBatched\
+#else\
+    cuS_st = cusolverSpDcsrqrsvBatched\
+#endif\
+   (/
+}}' "$FILE"
+
+echo "... Modifying PelePhysics/Source/Reactions/ReactorUtils.cpp"
+FILE="./lib/PelePhysics/Source/Reactions/ReactorUtils.cpp"
+sed -i '31{
   s|^[[:space:]]*//|//|;       # Keep already-commented lines as is
   t done
   s|^|//|
   :done
 }' "$FILE"
 
-echo "... Modifying PelePhysics/Reactions/ReactorCvode.cpp"
-FILE="../lib/PelePhysics/Reactions/ReactorCvode.cpp"
-sed -i '1168{/cusolver_status/{ # Keep already-commented lines as is
+echo "... Modifying PelePhysics/Source/Reactions/ReactorCvode.cpp"
+FILE="./lib/PelePhysics/Source/Reactions/ReactorCvode.cpp"
+sed -i '1164{/cusolver_status/{ # Keep already-commented lines as is
   s/.*/#ifdef AMREX_USE_FLOAT\
     cusolver_status = cusolverSpScsrqrBufferInfoBatched\
 #else\
@@ -33,15 +46,23 @@ sed -i '1168{/cusolver_status/{ # Keep already-commented lines as is
     (/
 }}' "$FILE"
 
-echo "... Modifying PelePhysics/Reactions/ReactorCvodePreconditioner.cpp"
-FILE="../lib/PelePhysics/Reactions/ReactorCvodePreconditioner.cpp"
-sed -i '115{/cuS_st/{ # Keep already-commented lines as is
-  s/.*/#ifdef AMREX_USE_FLOAT\
-    cuS_st = cusolverSpScsrqrsvBatched\
-#else\
-    cuS_st = cusolverSpDcsrqrsvBatched\
-#endif\
-   (/
+echo "... Modifying PelePhysics/Source/Reactions/ReactorCvode.cpp (Adding cleanup)"
+FILE="./lib/PelePhysics/Source/Reactions/ReactorCvode.cpp"
+sed -i '1355{/return (1);/{
+  s|.*|    //clean up even when error! \
+    N_VDestroy(y);\
+    CVodeFree(\&cvode_mem);\
+    if (LS != nullptr) {\
+      SUNLinSolFree(LS);\
+    }\
+    if (NLS != nullptr) {\
+      SUNNonlinSolFree(NLS);\
+    }\
+    if (A != nullptr) {\
+      SUNMatDestroy(A);\
+    }\
+    freeUserData(udata);\
+    return (1);|
 }}' "$FILE"
 
 echo "DONE"
